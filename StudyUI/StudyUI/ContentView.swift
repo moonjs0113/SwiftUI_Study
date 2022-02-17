444be4ea2efbb9d9ffc1045a5aa9c30617a87dca
Jongseek Moon <littlemoom@naver.com>
2022-02-17 15:00:26 +0900

    Start Study

---
+++ StudyUI/StudyUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudyUI
+//
+//  Created by 문종식 on 2022/02/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudyUI/StudyUI/StudyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyUIApp.swift
+//  StudyUI
+//
+//  Created by 문종식 on 2022/02/17.
+//
+
+import SwiftUI
+
+@main
+struct StudyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
