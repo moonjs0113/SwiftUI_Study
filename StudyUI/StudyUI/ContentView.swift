cd409fa4c27cd46c9f8f9db7aa1e53821ee567ce
Jongseek Moon <littlemoom@naver.com>
2022-02-17 15:27:21 +0900

    Add Form in StudyUI#1

--- StudyUI/StudyUI/ContentView.swift
+++ StudyUI/StudyUI/ContentView.swift
@@ -9,8 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // Form = TableView 맞는듯
+        // child View를 10개까지만 넣을 수 있는듯 11개가 넘어가면 오류
+        // -> Section이나 Group으로 묶으면 11개 이상도 가능함
+        Form {
+            Group {
+                // Text = UILabel
+                Text("Group, world #1").padding()
+                Text("Group, world #2 without padding()")
+                Text("Group, world #3 without padding()")
+            }
+            Section {
+                Group {
+                    Text("Group in Section")
+                }
+                Text("Section, world #4 without padding()")
+                Text("Section, world #5").padding()
+                Text("Section, world #6 without padding()")
+                Section {
+                    Text("Section in Section #1 without padding()")
+                    Text("Section in Section #2").padding()
+                }
+            }
+            Group {
+                Text("Group, world #7 without padding()")
+                Text("Group, world #8").padding()
+                Text("Group, world #9 without padding()")
+                Group {
+                    Text("Group in Group #1").padding()
+                    Text("Group in Group #2").padding()
+                    Text("Group in Group #3 without padding()")
+                }
+                Section {
+                    Text("Section in Group #1 without padding()")
+                }
+            }
+            Group {
+                Text("Group, world #10 without padding()")
+                Text("Group, world #11 without padding()")
+                Text("Group, world #12 without padding()")
+            }
+        }
     }
 }
 
