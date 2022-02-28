976551d6e978cc4a377ee9d6b11ef1f8c94b6366
Jongseek Moon <littlemoom@naver.com>
2022-02-28 22:58:37 +0900

    Add DatePicker

--- StudyUI/StudyUI/Consolidation3/Day26.swift
+++ StudyUI/StudyUI/Consolidation3/Day26.swift
@@ -9,12 +9,25 @@ import SwiftUI
 
 struct Day26: View {
     @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    
+    func exampleDates() {
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        let range = Date.now...tomorrow
+    }
     
     var body: some View {
         Stepper("\(self.sleepAmount) hours", value: self.$sleepAmount)
         Stepper("\(self.sleepAmount) hours\nin 4...12", value: self.$sleepAmount, in: 4...12)
         Stepper("\(self.sleepAmount) hours\nin 4...12, step: 0.25", value: self.$sleepAmount, in: 4...12, step: 0.25)
         Stepper("\(self.sleepAmount.formatted()) hours\nin 4...12, step: 0.25 with formatted()", value: self.$sleepAmount, in: 4...12, step: 0.25)
+        
+        DatePicker("Please enter a date", selection: $wakeUp)
+        DatePicker("", selection: $wakeUp)
+        DatePicker("Please enter a date", selection: $wakeUp)
+            .labelsHidden()
+        DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
     }
 }
 
