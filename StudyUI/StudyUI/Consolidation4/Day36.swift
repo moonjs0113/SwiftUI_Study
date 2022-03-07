b2618a189aa8b9393a4c33c6e2acbb129a1fc2ec
Jongseek Moon <littlemoom@naver.com>
2022-03-08 00:26:56 +0900

    End Day36

--- StudyUI/StudyUI/Consolidation4/Day36.swift
+++ StudyUI/StudyUI/Consolidation4/Day36.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+struct CodableUser: Codable {
+    var firstName: String
+    var lastName: String
+}
+
 class User: ObservableObject {
     @Published var firstName = "Bilbo"
     @Published var lastName = "Baggins"
@@ -32,12 +37,19 @@ struct Day36: View {
     @State private var numbers = [Int]()
     @State private var currentNumber = 1
     
+    @State private var tapCount: Int = UserDefaults.standard.integer(forKey: "Tap")
+    @AppStorage("tapCount") private var AppStorageTapCount = 0
+    
+    @State private var codableUser = CodableUser(firstName: "Taylor", lastName: "Swift")
+    @State private var loadCodableUser: CodableUser?
+    @State private var isLoadSuccess = false
+    
     func removeRows(at offsets: IndexSet) {
         self.numbers.remove(atOffsets: offsets)
     }
     
     var body: some View {
-        VStack {
+        VStack(spacing: 15) {
             Text("Your name is \(self.user.firstName) \(self.user.lastName)")
             TextField("First name", text: self.$user.firstName)
             TextField("Last name", text: self.$user.lastName)
@@ -58,6 +70,43 @@ struct Day36: View {
             Button("Add Number") {
                 self.numbers.append(self.currentNumber)
                 self.currentNumber += 1
+            }
+            
+            VStack(spacing: 10) {
+                Button("UserDefaults Tap count: \(self.tapCount)") {
+                    self.tapCount += 1
+                    UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+                }
+                
+                Button("AppStorage Tap count: \(self.AppStorageTapCount)") {
+                    self.AppStorageTapCount += 1
+                }
+                
+                Button("Save Codable User") {
+                    let encoder = JSONEncoder()
+                    if let data = try? encoder.encode(self.codableUser) {
+                        UserDefaults.standard.set(data, forKey: "UserData")
+                    }
+                }
+                Button("Load Codable User") {
+                    let decoder = JSONDecoder()
+                    if let userData = UserDefaults.standard.data(forKey: "UserData") {
+                        if let data = try? decoder.decode(CodableUser.self, from: userData) {
+                            self.loadCodableUser = data
+                            self.isLoadSuccess.toggle()
+                        }
+                    }
+                }
+                
+                if self.isLoadSuccess {
+                    Rectangle()
+                        .fill(.yellow)
+                        .frame(width: 250, height: 50)
+                        .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    if let codableUser = self.loadCodableUser {
+                        Text("Load name is \(codableUser.firstName) \(codableUser.lastName)")
+                    }
+                }
             }
         }
         .toolbar {
