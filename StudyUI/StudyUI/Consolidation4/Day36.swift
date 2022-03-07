6fc19bb62a8e6fe32c2b37332db4040b1424f143
Jongseek Moon <littlemoom@naver.com>
2022-03-08 00:08:31 +0900

    Day36 - ing

--- StudyUI/StudyUI/Consolidation4/Day36.swift
+++ StudyUI/StudyUI/Consolidation4/Day36.swift
@@ -7,18 +7,61 @@
 
 import SwiftUI
 
-struct User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
+class User: ObservableObject {
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
+}
+
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    let name: String
+    
+    var body: some View {
+        Text("Hello, \(self.name)")
+        Button("Dismiss") {
+            self.dismiss()
+        }
+    }
 }
 
 struct Day36: View {
-    @State private var user = User()
+    @StateObject private var user = User()
+    @State private var showingSheet = false
+    
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
+    func removeRows(at offsets: IndexSet) {
+        self.numbers.remove(atOffsets: offsets)
+    }
+    
     var body: some View {
         VStack {
             Text("Your name is \(self.user.firstName) \(self.user.lastName)")
             TextField("First name", text: self.$user.firstName)
             TextField("Last name", text: self.$user.lastName)
+            
+            Button("Show sheet") {
+                self.showingSheet.toggle()
+            }
+            .sheet(isPresented: self.$showingSheet) {
+                SecondView(name: "@m_oon_js")
+            }
+            
+            List {
+                ForEach(self.numbers, id: \.self) {
+                    Text("Row \($0)")
+                }
+                .onDelete(perform: self.removeRows(at:))
+            }
+            Button("Add Number") {
+                self.numbers.append(self.currentNumber)
+                self.currentNumber += 1
+            }
+        }
+        .toolbar {
+            EditButton()
         }
     }
 }
