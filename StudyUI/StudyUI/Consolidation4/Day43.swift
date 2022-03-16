b9aa3f55deb90f667bf27f66d197da29878acdfd
Jongseek Moon <littlemoom@naver.com>
2022-03-17 00:29:50 +0900

    End Day43

---
+++ StudyUI/StudyUI/Consolidation4/Day43.swift
@@ -0,0 +1,92 @@
+//
+//  Day43.swift
+//  StudyUI
+//
+//  Created by 문종식 on 2022/03/16.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: InsettableShape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    
+    var insetAmlunt = 0.0
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = self.startAngle - rotationAdjustment
+        let modifiedEnd = self.endAngle - rotationAdjustment
+        
+        var path = Path()
+//        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: self.startAngle, endAngle: self.endAngle, clockwise: self.clockwise)
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - self.insetAmlunt, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !self.clockwise)
+        
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmlunt += amount
+        return arc
+    }
+}
+
+struct Day43: View {
+    var body: some View {
+        ScrollView {
+            Path { path in
+                path.move(to: CGPoint(x: 200, y: 100))
+                path.addLine(to: CGPoint(x: 100, y: 300))
+                path.addLine(to: CGPoint(x: 300, y: 300))
+                path.addLine(to: CGPoint(x: 200, y: 100))
+                // 선 끝부분 붙여줌
+                path.closeSubpath()
+            }
+    //        .fill(.blue)
+    //        .stroke(.blue, lineWidth: 10)
+            .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            
+            Circle()
+                .stroke(.blue, lineWidth: 40)
+            
+            Circle()
+                .strokeBorder(.blue, lineWidth: 40)
+            
+            Triangle()
+                .fill(.red)
+                .frame(width: 30, height: 30)
+            
+            Triangle()
+                .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                .frame(width: 25, height: 25)
+            
+            Arc(startAngle: .degrees(0), endAngle: .degrees(100), clockwise: true)
+                .stroke(.blue, lineWidth: 10)
+                .frame(width: 100, height: 100)
+            
+            Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+                .stroke(.blue, lineWidth: 5)
+        }
+    }
+}
+
+struct Day43_Previews: PreviewProvider {
+    static var previews: some View {
+        Day43()
+    }
+}
--- StudyUI/StudyUI/MainView.swift
+++ StudyUI/StudyUI/MainView.swift
@@ -30,6 +30,7 @@ struct MainView: View {
                 Section("Consolidation IV") {
                     NavigationLink("Project 7", destination: Day36())
                     NavigationLink("Project 8", destination: Day39())
+                    NavigationLink("Project 9", destination: Day43())
                 }
             }
             .navigationTitle("SwiftUI Study App")
