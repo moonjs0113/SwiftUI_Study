27907d472ac1dda1dc8c7f9ba346aed148db9ca1
moonjs0113 <littlemoom@naver.com>
2022-03-22 23:44:48 +0900

    Add User Class in Day49

--- StudyUI/StudyUI/Consolidation5/Day49.swift
+++ StudyUI/StudyUI/Consolidation5/Day49.swift
@@ -7,6 +7,26 @@
 
 import SwiftUI
 
+
+
+class User: ObservableObject, Codable {
+    @Published var name = "Paul Hudson"
+    
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let contanier = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try contanier.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.name, forKey: .name)
+    }
+}
+
 struct Day49: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
