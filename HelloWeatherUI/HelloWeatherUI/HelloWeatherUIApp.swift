05086c9737d4cb5bdc9a67a10cad110b423c5d54
moonjs0113 <littlemoom@naver.com>
2022-07-04 16:42:36 +0900

    Add WeatherUI

---
+++ HelloWeatherUI/HelloWeatherUI/HelloWeatherUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWeatherUIApp.swift
+//  HelloWeatherUI
+//
+//  Created by Moon Jongseek on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct HelloWeatherUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
